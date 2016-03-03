4cc820900aa25f3e66909fac4a08449a51a60607
Sergio Barrera <sdbeng@gmail.com>
2016-03-02 22:19:53 -0800

    Set up the file structure and make webpack -w run

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 //configure our entry point
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: __dirname + '/app/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
 module.exports = {
 	entry: [
 		'./app/index.js'
-	]
+	],
+	module: {
+		loaders: [
+			{test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"}
+		]
+	},
+	output: {
+		filename: "index_bundle.js",
+		path: __dirname + '/dist'
+	},
+	plugins: [HTMLWebpackPluginConfig]
 }
